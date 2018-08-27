fd8a0800daf28e4d6804f04b3ed9b38bce7c2556
Igor Chernyshev <igorchern07@gmail.com>
2018-08-27 00:19:49 -0500

    db join

--- app/database/friends.js
+++ app/database/friends.js
@@ -1,6 +1,8 @@
 import connection from './connection';
 import faker from 'faker';
 
+const friendScores = [];
+const friends = [];
 const scores = [];
 
 const friendsQuery = `INSERT INTO friends (friend_name, avatar) VALUES ?`;
@@ -39,7 +41,7 @@ const newFriend = function(name, avatar) {
 
     //Generate  10 question_num and answer_num values associated with last added friend's id
     for (let i = 1; i <= 10; i++) {
-      scoresValues.push([maxId, i, scores[i] || randomScore()]);
+      scoresValues.push([maxId, i, friendScores[i] || randomScore()]);
     }
     connection.query(scoresQuery, [scoresValues], (err, result) => {
       if (err) {
@@ -51,4 +53,46 @@ const newFriend = function(name, avatar) {
 };
 
 //Run this function to create a new random friend
-// newFriend(randomName, randomAvatar);
+//newFriend(randomName, randomAvatar);
+
+const joinFriendsQuery = `SELECT id, friend_name, avatar FROM friends INNER JOIN scores on friends.id = scores.friend_id GROUP BY friends.id;`;
+
+const joinScoresQuery = `SELECT friend_id, question_num, answer_num from scores INNER JOIN friends on scores.friend_id = friends.id`;
+
+const joinQuery = `SELECT * FROM friends INNER JOIN scores on friends.id = scores.friend_id`;
+
+// connection.query(joinQuery, (err, result) => {
+//   result.forEach(item => {
+//     console.log(item.question_num);
+//     console.log(item.answer_num);
+//   });
+// });
+
+// connection.query(joinScoresQuery, (err, result) => {
+//   if (err) {
+//     throw err;
+//   }
+//   result.forEach(item => {
+//     const { friend_id, question_num, answer_num } = item;
+//     console.log('id:', friend_id);
+//     console.log('question_num:', question_num);
+//     console.log('answer_num:', answer_num);
+//   });
+// });
+
+// connection.query(joinFriendsQuery, (err, result) => {
+//   if (err) {
+//     throw err;
+//   }
+//   result.forEach((item, index) => {
+//     const { friend_name, avatar } = item;
+//     const newFriend = {
+//       name: friend_name,
+//       avatar: avatar
+//     };
+//     friends.push(newFriend);
+//     if (result.length === index + 1) {
+//       return friends;
+//     }
+//   });
+// });
