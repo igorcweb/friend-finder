53e7d3464cd6955d1c02e8749aa22c6b61c17717
Igor Chernyshev <igorchern07@gmail.com>
2018-09-02 20:12:52 -0500

    JAWSDB connection

--- app/database/connection.js
+++ app/database/connection.js
@@ -2,27 +2,32 @@ import dotenv from 'dotenv';
 dotenv.config();
 import mysql from 'mysql';
 
+const { DBUSER, DBPASS, JAWSDB_URL } = process.env;
+
 const dbConfig = {
   host: 'localhost',
   port: 3306,
-  user: process.env.DBUSER,
-  password: process.env.DBPASS,
+  user: DBUSER,
+  password: DBPASS,
   database: 'friend_finder'
 };
 
 let connection;
 
-// Heroku config
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (JAWSDB_URL) {
+  connection = mysql.createConnection(JAWSDB_URL);
+  console.log('JAWSDB');
 } else {
   connection = mysql.createConnection(dbConfig);
+  console.log('localhost');
 }
 
 connection.connect(err => {
   if (err) {
     console.log('Error: ', err);
     connection.end();
+  } else {
+    console.log('DB is connected');
   }
 });
 
